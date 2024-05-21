dec96e295258263f954fca1314a669b01eb2414d
SalsaNP777 <salsa.nputr777@gmail.com>
2024-05-21 19:12:52 +0700

    finish

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    int n = 5, column;
+    column = n * 5;
+
+    Console.WriteLine("\nOutput:");
+    for(int i = 1; i <= n; i++) {
+      for(int j = i; j <= column; j += n) {
+        Console.Write(j + " ");
+      }
+      Console.WriteLine();
+    }
+  }
+}
